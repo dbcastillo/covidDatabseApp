c218961fb38778894bfc155f7b65710a526c65e7
Castillo <905903@cognizant.com>
2021-04-27 15:49:58 -0500

    Create outline with psuedo-code & variables

--- src/main/java/com/covidDatabaseApp/CovidDatabaseApp.java
+++ src/main/java/com/covidDatabaseApp/CovidDatabaseApp.java
@@ -1,4 +1,9 @@
 package com.covidDatabaseApp;
 
 public class CovidDatabaseApp {
+
+    public static void main(String[] args{
+        // Ask how many new users we want to add
+        // Create n number of users
+    }
 }
--- src/main/java/com/covidDatabaseApp/User.java
+++ src/main/java/com/covidDatabaseApp/User.java
@@ -1,4 +1,23 @@
 package com.covidDatabaseApp;
 
 public class User {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private int userId;
+    private String services;
+    private int serviceBalance;
+    private int costOfService = 50;
+
+    // Constructor: prompt user to enter user's name and dateOfBirth
+
+    // Generate an ID
+
+    // Enroll in services
+
+    // View balance
+
+    // Pay for service
+
+    // Show status
 }
